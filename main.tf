3dab4a408e0122b8c1a2ccb4e7d3cdc45eec0705
Javier Romero Lancha <javiymarina04@gmail.com>
2025-04-10 11:47:15 +0200

    Evitar validación del ID de cuenta AWS con LocalStack

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 provider "aws" {
   region                      = "us-east-1"
-  access_key                  = "mock"
-  secret_key                  = "mock"
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   s3_use_path_style           = true
+
   endpoints {
     s3 = "http://localhost:4566"
   }
