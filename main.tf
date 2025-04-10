8bf10de8bea20f7b150df90f4cfd86acca891518
Javier Romero Lancha <javiymarina04@gmail.com>
2025-04-10 10:38:52 +0200

    Corregir error de argumento no soportado en provider

--- main.tf
+++ main.tf
@@ -4,14 +4,10 @@ provider "aws" {
   region     = "us-east-1"
 
   endpoints {
-    s3 = "http://127.0.0.1:4566"
+    s3                 = "http://127.0.0.1:4566"
+    s3_force_path_style = true
   }
 
   skip_credentials_validation = true
   skip_metadata_api_check     = true
-  s3_force_path_style         = true
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-localstack-bucket"
 }
